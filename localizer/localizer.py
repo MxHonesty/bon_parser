7e9f09a8fea8deaf70536da6c0c902d65723fb3d
Stelian <stoianstelian33@gmail.com>
2020-11-16 17:32:13 +0200

    image resize

--- localizer/localizer.py
+++ localizer/localizer.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import imutils
 from skimage.filters import threshold_local
+from PIL import Image
 
 
 class Localizer:
@@ -13,6 +14,35 @@ class Localizer:
         """ Clasa proceseaza imaginea data. 
             Date de intrare: Image img.
             Dade de iesire: Image. """
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
+        #img = Localizer.scale_image(img)  # scalam imaginea
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+        #gray = Localizer.remove_noise_and_smooth(gray)
+        #cv2.imshow('image',img)
+        #k = cv2.waitKey(0)
         return gray
+
+    @staticmethod
+    def scale_image(img):
+        """ Aduce imaginea la dimensiunile ideale. 
+            Date de intrare: imaginea initiala.
+            Date de iesire: imaginea scalata. """
+        img = Image.fromarray(img)
+        length_x = img.height  
+        width_y = img.width
+        factor = min(1, float(1024.0 / length_x))
+        size = int(factor * length_x), int(factor * width_y)
+        img = np.asarray(img)
+        img_resized = cv2.resize(img, size, Image.ANTIALIAS)
+        return img_resized
+
+    @staticmethod
+    def remove_noise_and_smooth(img):
+        """ Elimina noise din imagine. 
+            Date de intrare: img.
+            Date de iesire: imaginea preprocesata. """
+        filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
+        kernel = np.ones((1, 1), np.uint8)
+        opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+        or_image = cv2.bitwise_or(img, closing)
+        return or_image
